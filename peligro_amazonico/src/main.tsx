3b679574fb04526bd499bc6603655855be089207
karimsh2001 <karimsh2001@gmail.com>
2024-01-04 23:01:05 -0500

    router

--- peligro_amazonico/src/main.tsx
+++ peligro_amazonico/src/main.tsx
@@ -1,11 +1,20 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import 'tailwindcss/tailwind.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+
+// Registrar rutas
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <RouterProvider router={router} />
+  </React.StrictMode>
 )
