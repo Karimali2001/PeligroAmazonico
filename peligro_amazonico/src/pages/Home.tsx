d14d282eda73b9aae2ec8fac1f4522a00ccf2348
karimsh2001 <karimsh2001@gmail.com>
2024-01-22 06:57:26 -0400

    home

--- peligro_amazonico/src/pages/Home.tsx
+++ peligro_amazonico/src/pages/Home.tsx
@@ -10,14 +10,17 @@ const Home: React.FC = () => {
         window.location.href = '/jugadores';
     }
     return (
-        <div className='bg-[#FFB534]' style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%',overflow: 'auto' }}>
+        <div className='bg-[#FFB534]' style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', overflow: 'auto' }}>
             <AppBar />
-            <div className='m-3 relative h-full items-center'>
-                <img src={logo} alt="logo" />
-                <h1 className='text-white title'>¿Quieres aprender de forma divertida
-                    sobre los seres vivos peligrosos que
-                    habitan en el Amazonas?</h1>
-                <button className='principal-button mt-[10%]' onClick={onClick} style={{ marginTop: '8%', marginBottom: '20px' }}>Jugar</button>
+            <div className='m-3'>
+
+                <div className='m-auto flex items-center justify-center flex-col'>
+                    <img src={logo} alt="logo" />
+                    <h1 className='text-white title' style={{textAlign: 'center'}}>¿Quieres aprender de forma divertida
+                        sobre los seres vivos peligrosos que
+                        habitan en el Amazonas?</h1>
+                    <button className='principal-button mt-[10%]' onClick={onClick} style={{ marginTop: '8%', marginBottom: '20px' }}>Jugar</button>
+                </div>
             </div>
 
         </div>
