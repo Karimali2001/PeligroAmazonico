2974c5c739da662c4633ee490723b0980336a7f1
karimsh2001 <karimsh2001@gmail.com>
2024-01-22 12:30:32 -0400

    nombre de jugador

--- peligro_amazonico/src/pages/Menu.tsx
+++ peligro_amazonico/src/pages/Menu.tsx
@@ -7,10 +7,16 @@ const Menu: React.FC = () => {
 
     const history = createBrowserHistory(); //to pass elements between pages
 
+    const [playerName, setPlayerName] = useState('');
+
     const [isOnline, setIsOnline] = useState(false);
 
     const handleClickOnline = () => {
         setIsOnline(true);
+    }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPlayerName(event.target.value);
     }
 
     const handleClickLocal = () => {
@@ -18,12 +24,40 @@ const Menu: React.FC = () => {
         window.location.reload();
     }
 
+    const handleBack = () => {
+        setIsOnline(false);
+    }
+
+    const handleCreateGroup = () => {
+
+        if (playerName == '') {
+            alert('Ingrese un nombre de jugador');
+            return;
+        }
+
+        history.push('/crear-grupo');
+        window.location.reload();        
+    }
+
+    const handleJoinGroup = () => {
+
+        if (playerName == '') {
+            alert('Ingrese un nombre de jugador');
+            return;
+        }
+
+        history.push('/unirse-grupo');
+        window.location.reload();        
+    }
 
 
     return (
         <div className='bg-[#FFB534]' style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
+            
             <AppBar />
+            {isOnline &&  <a onClick={handleBack} className='m-3 text-white underline text-xl'> {"<"} Atrás</a>}
             <div className='m-3 flex flex-col h-full items-center'>
+                
                 <h1 className='title'>Seleccione modo de juego</h1>
                 <div className='flex-col flex h-auto w-full mt-[5%] space-y-2 items-center'>
                     {isOnline ? (
@@ -32,9 +66,12 @@ const Menu: React.FC = () => {
                             <input
                                 placeholder='Ingrese el nombre del jugador'
                                 className='rounded-full w-full border-2 border-gray-300 px-4 py-2 focus:outline-none focus:border-blue-500'
+                                value={playerName}
+                                onChange={handleInputChange}
                             />
-                            <button className='principal-button'>Crear Grupo</button>
-                            <button className='principal-button'>Unirse a Grupo</button>
+                            <button onClick={handleCreateGroup} className='principal-button'>Crear Grupo</button>
+                            <button onClick={handleJoinGroup} className='principal-button'>Unirse a Grupo</button>
+                            
                         </>
                     ) : (
                         // Render the original buttons when isOnline is false
