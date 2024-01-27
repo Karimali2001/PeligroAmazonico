007d28a11f24f22e3b054dd391e673220f0b7b4c
karimsh2001 <karimsh2001@gmail.com>
2024-01-27 14:25:27 -0400

    imagenes en multiplayer

--- peligro_amazonico/src/pages/Multiplayers.tsx
+++ peligro_amazonico/src/pages/Multiplayers.tsx
@@ -3,7 +3,45 @@ import { createBrowserHistory } from 'history';
 import AppBar from '../components/AppBar';
 import "../App.css"
 import { FaPlus, FaMinus } from 'react-icons/fa';
-import { images as imgs } from './images';
+import arania from '../assets/game-imgs/arania.svg';
+import escorpion from "../assets/game-imgs/escorpion.svg";
+import anaconda from "../assets/game-imgs/anaconda.svg";
+import caiman from "../assets/game-imgs/caiman.svg";
+import anguila from "../assets/game-imgs/anguila.svg";
+import vibora from "../assets/game-imgs/serpiente.svg";
+import pirania from "../assets/game-imgs/pirania.svg";
+import rana from "../assets/game-imgs/rana.svg";
+import chucho from "../assets/game-imgs/chucho.svg";
+import jaguar from "../assets/game-imgs/jaguar.svg";
+import ciempies from "../assets/game-imgs/ciempies.svg";
+import mosquito from "../assets/game-imgs/mosquito.svg";
+import candiru from "../assets/game-imgs/candiru.svg";
+import tarantula from "../assets/game-imgs/tarantula.svg";
+import trompeta from "../assets/game-imgs/trompeta.svg";
+import strychnos from "../assets/game-imgs/strychnos.svg";
+import curare from "../assets/game-imgs/curare.svg";
+import shuar from "../assets/game-imgs/shuar.svg";
+
+export const imgs = [
+    { name: "Araña bananera", path: arania, content: "de las arañas más venenosas del mundo.", wikipedia: "https://es.wikipedia.org/wiki/Phoneutria"},
+    { name: "Escorpiones Tityus", path: escorpion, content: "Son los más venenosos de América.", wikipedia: "https://es.wikipedia.org/wiki/Tityus"},
+    { name: "Anaconda verde", path: anaconda, content: "Es la serpiente más grande del mundo.", wikipedia: "https://es.wikipedia.org/wiki/Eunectes_murinus"},
+    { name: "Caimán", path: caiman, content: "reptil que habita en las aguas dulces de la Amazonía.", wikipedia: "https://es.wikipedia.org/wiki/Caiman"},
+    { name: "Anguila eléctrica", path: anguila, content: "puede generar descargas eléctricas de hasta 600 voltios.", wikipedia: "https://es.wikipedia.org/wiki/Electrophorus_electricus"},
+    { name: "Víbora barba amarilla", path: vibora, content: "de las serpientes más venenosas del mundo.", wikipedia: "https://es.wikipedia.org/wiki/Bothrops_asper"},
+    { name: "Piraña amazónica", path: pirania, content: "pez carnívoro que habita en las aguas dulces.", wikipedia: "https://es.wikipedia.org/wiki/Pira%C3%B1a"},
+    { name: "Rana punta de flecha", path: rana, content: "Las tribus indígenas usan su veneno en sus flechas.", wikipedia: "https://es.wikipedia.org/wiki/Dendrobates"},
+    { name: "Chuchos de agua dulce", path: chucho, content: "Viven agua dulce y tienen un aguijón venenoso.", wikipedia: "https://es.wikipedia.org/wiki/Potamotrygonidae"},
+    { name: "Jaguar", path: jaguar, content: "Es el felino con la mandíbula más fuerte del mundo.", wikipedia: "https://es.wikipedia.org/wiki/Panthera_onca"},
+    { name: "Ciempiés gigante amazónico", path: ciempies, content: "Es el ciempiés más grande del mundo.", wikipedia: "https://es.wikipedia.org/wiki/Scolopendra_gigantea"},
+    { name: "Mosquito", path: mosquito, content: "Es el animal que mata más personas cada año.", wikipedia: "https://es.wikipedia.org/wiki/Mosquito"},
+    { name: "Candirú", path: candiru, content: "pez que puede entrar por los orificios del cuerpo humano.", wikipedia: "https://es.wikipedia.org/wiki/Vandellia_cirrhosa"},
+    { name: "Tarántula Goliat", path: tarantula, content: "Es la araña más grande del mundo.", wikipedia: "https://es.wikipedia.org/wiki/Theraphosa_blondi"},
+    { name: "Trompeta de Angel", path: trompeta, content: "planta que se usa para envenenar dardos.", wikipedia: "https://es.wikipedia.org/wiki/Brugmansia"},
+    { name: "Strychnos", path: strychnos, content: "planta que se usa para envenenar dardos.", wikipedia: "https://es.wikipedia.org/wiki/Strychnos"},
+    { name: "Curare", path: curare, content: "planta que se usa para envenenar dardos.", wikipedia: "https://es.wikipedia.org/wiki/Curare"},
+    { name: "Tribu Shuar", path: shuar, content: "Es considerada la tribu indígena más peligrosa del amazonas.", wikipedia: "https://es.wikipedia.org/wiki/Shuar"},
+];
 
 const Multiplayers: React.FC = () => {
     const [inputList, setInputList] = useState([{ playerName: "" }, { playerName: "" }]);
